41107f69e52bbfdfc449123c5a3c3b64a1058d4b
alperenf <fatihbab34@gmail.com>
2022-04-10 11:23:07 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.pekversicherung {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens versicherung to javafx.fxml;
+    exports versicherung;
+}
